f53b8e60e13a1a5250cf5bde2bd6ce9cd43ea892
전재홍 <jeonjaehong@jeonjaehong-ui-MacBookPro.local>
2021-08-22 15:57:33 +0900

    jwt start

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,11 +4,14 @@ import { Repository } from 'typeorm';
 import { CreateAccountInput } from './dtos/create-account.dto';
 import { LoginInput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
+import * as jwt from 'jsonwebtoken';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly user: Repository<User>,
+    private readonly config: ConfigService,
   ) {}
 
   async createAccount({
@@ -54,9 +57,10 @@ export class UsersService {
           error: 'Wrong password',
         };
       }
+      const token = jwt.sign({ id: user.id }, this.config.get('SECRET_KEY'));
       return {
         ok: true,
-        token: 'lalalalal',
+        token,
       };
     } catch (error) {
       return {
