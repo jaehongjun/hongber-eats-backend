1d3265ec325c29b86718f20498a86b252c9d6269
전재홍 <jeonjaehong@jeonjaehong-ui-MacBookPro.local>
2021-08-22 15:02:28 +0900

    add create account & login

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,15 +1,50 @@
-import { Query, Resolver } from "@nestjs/graphql";
-import { User } from "./entities/user.entity";
-import { UsersService } from "./users.service";
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import {
+  CreateAccountInput,
+  CreateAccountOutput,
+} from './dtos/create-account.dto';
+import { LoginInput, LoginOutput } from './dtos/login.dto';
+import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
 
-@Resolver(of => User)
+@Resolver((of) => User)
 export class usersResolver {
-    constructor(
-        private readonly usersService: UsersService
-    ) { }
+  constructor(private readonly usersService: UsersService) {}
 
-    @Query(returns => Boolean)
-    hi() {
-        return true
+  @Query((returns) => Boolean)
+  hi() {
+    return true;
+  }
+
+  @Mutation((returns) => CreateAccountOutput)
+  async createAccount(
+    @Args('input') createAccountInput: CreateAccountInput,
+  ): Promise<CreateAccountOutput> {
+    try {
+      const { ok, error } = await this.usersService.createAccount(
+        createAccountInput,
+      );
+      return {
+        ok,
+        error,
+      };
+    } catch (error) {
+      return {
+        error,
+        ok: false,
+      };
     }
+  }
+
+  @Mutation((returns) => LoginOutput)
+  async login(@Args('input') loginInput: LoginInput): Promise<LoginOutput> {
+    try {
+      return this.usersService.login(loginInput);
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,68 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-import { User } from "./entities/user.entity";
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateAccountInput } from './dtos/create-account.dto';
+import { LoginInput } from './dtos/login.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-    constructor(
-        @InjectRepository(User) private readonly user: Repository<User>
-    ) { }
+  constructor(
+    @InjectRepository(User) private readonly user: Repository<User>,
+  ) {}
+
+  async createAccount({
+    email,
+    password,
+    role,
+  }: CreateAccountInput): Promise<{ ok: boolean; error?: string }> {
+    // check new user
+    // create user & hash the password
+
+    try {
+      const exists = await this.user.findOne({ email });
+      if (exists) {
+        // make error
+        return { ok: false, error: 'There is a user with that email already' };
+      }
+      await this.user.save(this.user.create({ email, password, role }));
+      return { ok: true };
+    } catch (e) {
+      return { ok: false, error: "Couldn't create account" };
+    }
+  }
+
+  async login({
+    email,
+    password,
+  }: LoginInput): Promise<{ ok: boolean; error?: string; token?: string }> {
+    // find the user with the email
+    // check if the passwod is correct
+    // make a JWT and give to the user
+    try {
+      const user = await this.user.findOne({ email });
+      if (!user) {
+        return {
+          ok: false,
+          error: 'User not found',
+        };
+      }
+      const passwordCorrect = await user.checkPassword(password);
+      if (!passwordCorrect) {
+        return {
+          ok: false,
+          error: 'Wrong password',
+        };
+      }
+      return {
+        ok: true,
+        token: 'lalalalal',
+      };
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
+  }
 }
